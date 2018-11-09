9883653f49c7e55223d2f289775593809bf58b07
Abhinav Anand <abhinav_anand@live.com>
2018-11-10 02:31:25 +0530

    Plot performance of various impl. of kruskal with increasing i/p sizes

---
+++ src/main.py
@@ -0,0 +1,88 @@
+import timeit
+
+import kruskal_list_sort as kruskal_list_sort
+import kruskal_veb as kruskal_veb
+import kruskal_fib_heap as kruskal_fib_heap
+
+
+def timeit_kruskal_list_sort(V, E, edges):
+    g = kruskal_list_sort.Graph(V)
+    for edge in edges:
+        u, v, w = edge
+        g.insert_edge(u, v, w)
+
+    g.kruskal()
+
+
+def timeit_kruskal_veb(V, E, edges):
+    g = kruskal_veb.Graph(V)
+    for edge in edges:
+        u, v, w = edge
+        g.insert_edge(u, v, w)
+
+    g.kruskal()
+
+
+def timeit_kruskal_fib_heap(V, E, edges):
+    g = kruskal_fib_heap.Graph(V)
+    for edge in edges:
+        u, v, w = edge
+        g.insert_edge(u, v, w)
+
+    g.kruskal()
+
+
+if __name__ == "__main__":
+    # setup code
+    files = [
+        "tests/input/1",
+        "tests/input/2",
+        "tests/input/3",
+        "tests/input/4",
+        "tests/input/5",
+        "tests/input/6",
+        "tests/input/7",
+        "tests/input/8",
+        "tests/input/9",
+        "tests/input/10",
+    ]
+    num_list = [10, 21, 55, 105, 153, 300, 630, 1225, 2556, 4950]
+    x_list, y_list, z_list = [], [], []
+
+    for filename in files:
+        fp = open(filename)
+        V, E = map(int, fp.readline().split())
+        edges = []
+        for _ in range(E):
+            u, v, w = map(int, fp.readline().split())
+            edges.append((u, v, w))
+        fp.close()
+
+        num = 100
+
+        x = timeit.Timer('timeit_kruskal_list_sort(V,E,edges)',
+                         setup="from __main__ import timeit_kruskal_list_sort",
+                         globals=globals()).timeit(number=num)
+        print(x)
+        x_list.append(x)
+
+        y = timeit.Timer('timeit_kruskal_veb(V,E,edges)',
+                         setup="from __main__ import timeit_kruskal_veb",
+                         globals=globals()).timeit(number=num)
+        print(y)
+        y_list.append(y)
+
+        z = timeit.Timer('timeit_kruskal_fib_heap(V,E,edges)',
+                         setup="from __main__ import timeit_kruskal_fib_heap",
+                         globals=globals()).timeit(number=num)
+        print(z)
+        z_list.append(z)
+
+        print()
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(num_list, x_list)
+    plt.plot(num_list, y_list)
+    plt.plot(num_list, z_list)
+    plt.show()
