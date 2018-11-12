7f6eb8a95d1b493cd39ee2914bb05fdc4e9a5722
Abhinav Anand <abhinav_anand@live.com>
2018-11-12 20:30:59 +0530

    Update test_generator

--- src/utils/test_generator.py
+++ src/utils/test_generator.py
@@ -5,7 +5,7 @@ def generate(V, E, max_weight=None):
     print(V, E)  # writes: V E
     count = 0
     while count < E:
-        w = random.randint(1, max_weight or E - 1)  # generate random weight in the range [1 ... E-1]
+        w = random.randint(1, 1023)  # generate random weight in the range [1 ... E-1]
         u = random.randint(0, V - 1)
         v = random.randint(0, V - 1)
         print(u, v, w)
@@ -17,7 +17,7 @@ def generate_sparse(V):
     generate(V, E)
 
 
-def generate_dense(V, max_weight):
+def generate_dense(V, max_weight=None):
     E = V * (V - 1) // 2
     generate(V, E, max_weight)
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     filename = input("Enter file name in which to write: ")
     V = int(input("Enter number of vertices of graph: "))
-    max_weight = int(input("Enter maximum edge weight: "))
+    # max_weight = int(input("Enter maximum edge weight: "))
     if filename:
-        sys.stdout = open(filename, 'wt')
-        generate_dense(V, max_weight)
+        sys.stdout = open("tests/input/" + filename, 'wt')
+        generate_dense(V)
